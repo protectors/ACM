0a3ba1c52afec0386b3e689ea58446352e8a272f
zfprotectors <1176136493@qq.com>
2021-06-03 15:54:45 +0800

    #位运算#集合#哈希表

--- LeetCode/LeetCode136-只出现一次的数字.cpp
+++ LeetCode/LeetCode136-只出现一次的数字.cpp
@@ -8,3 +8,31 @@ public:
         return res;
     }
 };
+
+/**
+* 方法二: 哈希表
+*
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int, int> mp;
+        for(int i:nums){
+            mp[i]++;
+        }
+        int res;
+        for(auto [first, second]:mp){
+            // cout<<first<<" "<<second<<endl;
+            if(second==1) res=first;
+        }
+        return res;
+    }
+};
+*/
+
+/**
+ * 方法三: 集合(python3)
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return sum(set(nums))*2 - sum(nums)
+ */
+
